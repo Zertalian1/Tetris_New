e750f71fe0ded959674aece14b1aeafb6aaf896a
Курдюков Данил <zertalian@mail.ru>
2022-03-24 16:30:16 +0700

    /

---
+++ src/main/java/com/example/tetris_new/model/MainMenu.java
@@ -0,0 +1,13 @@
+package com.example.tetris_new.model;
+
+public class MainMenu {
+
+    public int show(int XMAX, int YMAX){
+        System.out.println("1) Играть");
+        System.out.println("2) Таблица Лидеров");
+        System.out.println("3) Выход");
+        return XMAX;
+    };
+
+    enum
+}
